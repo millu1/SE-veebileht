15abd22bddf37eceaa1e5e7634d1fa759553b6e6
kristjansaluvere <saluverekristjan@gmail.com>
2025-11-30 19:15:04 +0200

    Pildid + animatsioon

--- script.js
+++ script.js
@@ -1,8 +1,50 @@
-/* Allikas: https://codepen.io/aaroniker/pen/abzbRvo */
 
+/* Allikas: https://codepen.io/aaroniker/pen/abzbRvo */
+/* Nupu animatsioon */
 document.querySelectorAll('.button').forEach(button => {
     button.innerHTML =
         '<div><span>' +
-        button.textContent.trim().split('').join('</span><span>') +
+        button.textContent.trim() +
         '</span></div>';
 });
+
+
+/* Logo animatsioon */
+
+const logo = document.querySelector('.logo');
+
+const pildid = [
+    'pildid/1.png', 
+    'pildid/2.png', 
+    'pildid/3.png', 
+    'pildid/4.png'
+];
+
+let indeks = 0;
+let taimer = null; 
+
+function vahetaPilti() {
+    indeks++;
+    
+    if (indeks >= pildid.length) {
+        indeks = 0;
+    }
+    logo.src = pildid[indeks];
+}
+
+if (logo) {
+
+ 
+    logo.addEventListener('mouseenter', () => {
+
+        taimer = setInterval(vahetaPilti, 500);
+    });
+
+    logo.addEventListener('mouseleave', () => {
+
+        clearInterval(taimer);
+
+        indeks = 0;
+        logo.src = pildid[0]; 
+    });
+}
