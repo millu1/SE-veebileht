8d0b17ff43c7ab6ef55855bb36c1ad741724f3ea
Semilla <semilla.tuberg@gmail.com>
2025-11-30 19:34:09 +0200

    logo suurendus

--- script.js
+++ script.js
@@ -14,10 +14,10 @@ document.querySelectorAll('.button').forEach(button => {
 const logo = document.querySelector('.logo');
 
 const pildid = [
-    'pildid/1.png', 
-    'pildid/2.png', 
-    'pildid/3.png', 
-    'pildid/4.png'
+    'pildid/logo2.jpg', 
+    'pildid/logo3.jpg', 
+    'pildid/logo4.jpg', 
+    'pildid/logo1.jpg'
 ];
 
 let indeks = 0;
